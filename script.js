cb50bf216e16af269ca2d480fa733c8113e11a3d
Jonathan Hoyos <158219709+Jonathann-h@users.noreply.github.com>
2024-06-18 17:37:50 -0400

    Actualizacion del CSS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    //Referencias a los botones
+    // Referencias a los elementos del DOM una vez que se carga el DOM
     const startButton = document.getElementById('start-btn');
     const stopButton = document.getElementById('stop-btn');
     const clearButton = document.getElementById('clear-btn');
@@ -9,22 +9,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
     if (!('webkitSpeechRecognition' in window)) {
         alert("Tu navegador no soporta la API de reconocimiento de voz. :(");
     } else {
-        //Se crea una instancia de webkitSpeechRecognition
-        const recognition = new webkitSpeechRecognition();
-
         // Configuración del reconocimiento de voz
+        const recognition = new webkitSpeechRecognition();
         recognition.continuous = true; // Continuar reconociendo incluso después de pausas
         recognition.interimResults = false; // Mostrar resultados solo cuando el discurso se haya completado
         recognition.lang = 'es-ES'; // Configurar el idioma en español
 
-        //Evento para cuando se inicia el reconocimiento de voz
+        // Referencia al contenedor de puntos una vez que se carga el DOM
+        const dotsContainer = document.querySelector('.dots-container');
+
+        // Evento para cuando se inicia el reconocimiento de voz
         recognition.onstart = () => {
-            startButton.disabled = true;    //Deshabilitar el Boton de Inicio
-            stopButton.disabled = false;    //Habilitar el Boton de Detener
+            startButton.disabled = true;
+            stopButton.disabled = false;
+            dotsContainer.style.display = 'flex';
             console.log('Reconocimiento de voz iniciado');
         };
 
-        //Evento para cuando se obtiene un resultado del reconocimiento de voz
+        // Evento para cuando se obtiene un resultado del reconocimiento de voz
         recognition.onresult = (event) => {
             let interimTranscript = '';
             for (let i = event.resultIndex; i < event.results.length; ++i) {
@@ -37,34 +39,36 @@ document.addEventListener('DOMContentLoaded', (event) => {
             console.log('Resultado:', interimTranscript);
         };
 
-        //Evento para un caso de error
+        // Evento para un caso de error
         recognition.onerror = (event) => {
             console.error('Error en el reconocimiento de voz:', event.error);
         };
 
-        //Evento para cuando se detiene el reconocimiento de voz
+        // Evento para cuando se detiene el reconocimiento de voz
         recognition.onend = () => {
-            startButton.disabled = false;   //Se habilita de vuelta el boton de Inicio
-            stopButton.disabled = true;     //Se deshabilita el Boton de Detener
+            startButton.disabled = false;
+            stopButton.disabled = true;
+            dotsContainer.style.display = 'none';
             console.log('Reconocimiento de voz detenido');
         };
 
-        //Iniciar el reconocimiento de voz al hacer click en el boton Inicio
+        // Iniciar el reconocimiento de voz al hacer click en el boton Inicio
         startButton.addEventListener('click', () => {
             recognition.start();
         });
 
-        //Detener el reconocimiento de voz al hacer click en Detener
+        // Detener el reconocimiento de voz al hacer click en Detener
         stopButton.addEventListener('click', () => {
             recognition.stop();
         });
 
-        //Limpiar el contenido del campo del texto
+        // Limpiar el contenido del campo del texto
         clearButton.addEventListener('click', () => {
             resultText.value = '';
         });
 
-        // Inicialmente deshabilitar el botón de detener
+        // Inicialmente deshabilitar el botón de detener y ocultar el contenedor de puntos
         stopButton.disabled = true;
+        dotsContainer.style.display = 'none';
     }
 });
